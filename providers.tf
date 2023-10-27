c6063a27981ab4cc1d3fc45ee46760ad2ba692ca
Jane <148792696+JamonAppleberg@users.noreply.github.com>
2023-10-27 17:38:10 +0300

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      # Используем локальное зеркало Облака КРОК
+      # как источник загрузки провайдера c2devel/croccloud
+      source  = "hc-registry.website.cloud.croc.ru/c2devel/croccloud"
+      version = "4.14.0-CROC7"
+    }
+  }
+}
+# Подключаем и настраиваем провайдера для работы
+# со всеми сервисами Облака КРОК, кроме объектного хранилища
+provider "aws" {
+  endpoints {
+    ec2 = "https://api.cloud.croc.ru"
+    elbv2 = "https://elb.cloud.croc.ru"
+  }
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+  insecure   = false
+  access_key = "access_key"
+  secret_key = "secret_key"
+  region     = "croc"
+}
